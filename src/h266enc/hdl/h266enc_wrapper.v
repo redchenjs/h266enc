ced2be48fc912b4c7905ef065ce0b68af524b0f4
Jack Chen <redchenjs@live.com>
2022-11-01 12:07:56 +0800

    src: add h266enc top module

---
+++ src/h266enc/hdl/h266enc_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
+//Date        : Tue Nov  1 15:11:05 2022
+//Host        : ThinkBook running 64-bit Arch Linux
+//Command     : generate_target h266enc_wrapper.bd
+//Design      : h266enc_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module h266enc_wrapper
+   ();
+
+
+  h266enc h266enc_i
+       ();
+endmodule
