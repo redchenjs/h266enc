8156ba88d7dd144471f28fcf8ff6739aaa02bdd1
Jack Chen <redchenjs@live.com>
2023-04-26 13:05:48 +0800

    src: update axi_h266enc clock

--- src/h266enc/hdl/h266enc_wrapper.v
+++ src/h266enc/hdl/h266enc_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2023 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2022.2.2 (win64) Build 3788238 Tue Feb 21 20:00:34 MST 2023
-//Date        : Wed Apr 26 11:56:11 2023
+//Date        : Wed Apr 26 12:49:15 2023
 //Host        : ThinPC running 64-bit major release  (build 9200)
 //Command     : generate_target h266enc_wrapper.bd
 //Design      : h266enc_wrapper
